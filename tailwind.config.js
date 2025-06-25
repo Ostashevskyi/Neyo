7ee001b3b50506d5845c8ef4dc69d83d4e68b3de
Yurii Ostashevskyi <xpeke2015@gmail.com>
2025-06-25 21:15:55 +0300

    connect tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary_black: 'var(--primary-black)'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
